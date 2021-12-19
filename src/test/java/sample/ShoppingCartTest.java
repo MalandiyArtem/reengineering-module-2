e4baebeb635fc1fc9f5c39789d16745c8c474e0f
MalandiyArtem <malandiyartem@gmail.com>
2021-12-19 22:12:39 +0200

    (test) Add calculateDiscount

--- src/test/java/sample/ShoppingCartTest.java
+++ src/test/java/sample/ShoppingCartTest.java
@@ -26,4 +26,24 @@ public class ShoppingCartTest {
         ShoppingCart.appendFormatted(sb, "SomeLine", -1, 15);
         assertEquals("SomeLine        ", sb.toString());
     }
+
+    @Test
+    public void calculateDiscount() {
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 500));
+        assertEquals(73, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 30));
+        assertEquals(71, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 10));
+        assertEquals(70, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 9));
+        assertEquals(70, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 1));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 20));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 10));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 1));
+        assertEquals(2, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 20));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 0));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 500));
+        assertEquals(53, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 30));
+        assertEquals(51, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 10));
+        assertEquals(50, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 9));
+        assertEquals(50, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 2));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 1));
+    }
 }
